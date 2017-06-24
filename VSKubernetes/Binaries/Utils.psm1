ca7fefa0503c5d0c26539cedd3f63bdd1c40180d
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2017-06-24 21:52:50 +0300

    Add Utils.psm1

---
+++ VSKubernetes/Binaries/Utils.psm1
@@ -0,0 +1,46 @@
+﻿$ErrorActionPreference = "Stop"
+
+function Exec
+{
+    param (
+        [Parameter(Position=0, Mandatory=1)]
+        [scriptblock]$Command
+    )
+    & $Command
+    if ($LastExitCode -ne 0) {
+        throw "Execution failed:`n$Command"
+    }
+}
+
+function Retry
+{
+    param (
+        [Parameter(Position=0, Mandatory=1)]
+        [scriptblock]$Command,
+        [Parameter(Position=1)]
+        [int]$SleepInterval = 1,
+        [Parameter(Position=2)]
+        [int]$Retries = 60
+     )
+
+    for ($i=1; $i -le $Retries; $i++) {
+        try {
+            Exec $Command
+            break
+        } catch {
+            Start-Sleep $SleepInterval
+            if ($i -eq $Retries) {
+                throw
+            }
+        }
+    }
+}
+
+function GetVMSwitchName()
+{
+    $vmSwitches = (Get-VMSwitch -SwitchType External)
+    if (!$vmSwitches) {
+        throw "Please create an external Hyper-V VM switch"
+    }
+    return $vmSwitches[0].Name
+}
